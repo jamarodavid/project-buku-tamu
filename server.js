3051b6031b91056a54a5c0194d97f908f0b2768c
raditya ananda <jamarodavid51@gmail.com>
2023-02-23 19:13:46 +0700

    update crud and ejs

--- server.js
+++ server.js
@@ -1,35 +1,81 @@
+const path = require("path");
 const express = require("express");
+const ejs = require("ejs");
+const bodyParser = require("body-parser");
 const mysql = require("mysql");
-
 const app = express();
 
-app.set("view engine", "ejs");
-app.set("views", "views");
-
-const db = mysql.createConnection({
+const connection = mysql.createConnection({
 	host: "localhost",
-	database: "data_kunjungan_rm",
 	user: "root",
 	password: "",
+	database: "node_crud",
 });
 
-db.connect((err) => {
-	if (err) throw err;
-	console.log("database connected ...");
+connection.connect(function (error) {
+	if (!!error) console.log(error);
+	else console.log("Database Connected!");
+});
 
-	const sql = "SELECT * FROM kedatangan";
-	db.query(sql, (err, result) => {
-		const users = JSON.parse(JSON.stringify(result));
-		console.log("hasil database -> ", users);
-		app.get("/", (req, res) => {
-			res.render("register", {
-				users: users,
-				title: "Register Rumah Makan Nusantara",
-			});
+//set views file
+app.set("views", path.join(__dirname, "views"));
+
+//set view engine
+app.set("view engine", "ejs");
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.get("/", (req, res) => {
+	// res.send('CRUD Operation using NodeJS / ExpressJS / MySQL');
+	let sql = "SELECT * FROM users";
+	let query = connection.query(sql, (err, rows) => {
+		if (err) throw err;
+		res.render("user_index", {
+			title: "CRUD Operation using NodeJS / ExpressJS / MySQL",
+			users: rows,
 		});
 	});
 });
 
+app.get("/add", (req, res) => {
+	res.render("user_add", {
+		title: "Isi form register di bawah ini",
+	});
+});
+
+app.post("/save", (req, res) => {
+	let data = {
+		nama: req.body.nama,
+		kontak: req.body.kontak,
+		waktu: req.body.waktu,
+		abjad_meja: req.body.abjad_meja,
+		nomor_meja: req.body.nomor_meja,
+	};
+	let sql = "INSERT INTO users SET ?";
+	let query = connection.query(sql, data, (err, results) => {
+		if (err) throw err;
+		res.redirect("/");
+	});
+});
+
+app.get("/edit/:userId", (req, res) => {
+	const userId = req.params.userId;
+	let sql = `Select * from users where id = ${userId}`;
+	let query = connection.query(sql, (err, result) => {
+		if (err) throw err;
+		res.render("user_edit", {
+			title: "CRUD Operation using NodeJS / ExpressJS / MySQL",
+			user: result[0],
+		});
+	});
+});
+
+// link pages
+app.get("/promo", (req, res) => {
+	res.render("promo");
+});
+
+// Server Listening
 app.listen(3000, () => {
-	console.log("Server sudah dimulai");
+	console.log("Server is running at port 3000");
 });
